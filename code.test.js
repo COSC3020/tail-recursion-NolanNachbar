16afd3fbce65daed850f6d35b2679325de7c2b00
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-10-30 16:06:05 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,1 +1,16 @@
-//test
+const fs = require('fs');
+const jsc = require('jsverify');
+
+eval(fs.readFileSync('code.js')+'');
+
+function fib(n) {
+  if (n <= 2) return 1;
+  else return fib(n-1) + fib(n-2);
+}
+
+const test =
+    jsc.forall("nat", function(n) {
+        return JSON.stringify(fib(n)) ==
+            JSON.stringify(fibTest(n));
+    });
+jsc.assert(test);
