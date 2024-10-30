7e6246533a789f439f6005768a2030691ee64523
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-10-30 16:22:45 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,9 @@
-function fib(n){
-  function _fib(n, acc){
-    if (n <= 2) return 1;
-    return _fib(n - 1, acc + n);
-  } 
-  return _fib(n, 0);
-}
 
+function fib(n){
+  function _fib(n2, acc, acc2){
+    if (n2 <= 1) return acc2;
+    return _fib(n2 - 1, acc + acc2, acc);
+  } 
+  
+  return _fib(n, 1, 1);
+}
